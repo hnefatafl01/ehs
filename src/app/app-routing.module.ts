aec525df63b7709e00771fb268eb76f7401584e3
Kevin Erickson <kevin.erick01@gmail.com>
2017-10-18 10:59:51 -0600

    skeleton done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
-import { AppComponent } from './app.component';
-import { AnnouncementsComponent } from './announcements/announcements.component';
-import { HomeComponent } from './home/home.component';
-// import { AppModule } from './app.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+
 const appRoutes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: 'home',
-  //   pathMatch: 'full'
-  // },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    pathMatch: 'full'
+  },
   {
     path: '', component: HomeComponent, children: [
       { path: '', component: AnnouncementsComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { MaterializeModule } from 'ng2-materialize';
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { AnnouncementsComponent } from './announcements/announcements.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +18,14 @@ import { AnnouncementsComponent } from './announcements/announcements.component'
     HeaderComponent,
     HomeComponent,
     FooterComponent,
-    AnnouncementsComponent
+    AnnouncementsComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterializeModule.forRoot(),
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
