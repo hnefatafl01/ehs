572f858d775b628389cf2a48bedaa92de7d0c656
Kevin Erickson <kevin.erick01@gmail.com>
2017-10-20 17:32:15 -0600

    user changes announcements

--- src/app/announcements/announcements.component.ts
+++ src/app/announcements/announcements.component.ts
@@ -1,50 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
-// import Rx from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 
+import { Announcement } from './../shared/announcement.model';
 import { DataService } from './../shared/data.service';
 
 @Component({
   selector: 'app-announcements',
   templateUrl: './announcements.component.html',
   styleUrls: ['./announcements.component.css'],
-  // animations: [
-  //   trigger(
-  //     'slideInRight',
-  //     [
-  //       transition(
-  //         ':enter', [
-  //           style({transform: 'translateX(100%)', opacity: 0}),
-  //           animate('200ms', style({transform: 'translateX(0)', opacity: 1}))
-  //         ]
-  //       ),
-  //       transition(
-  //         ':leave', [
-  //           style({transform: 'translateX(0)', 'opacity': 1}),
-  //           animate('500ms', style({transform: 'translateX(-100%)', opacity: 0}))
-  //         ]
-  //       )
-  //     ])
-  // ]
+  animations: [
+    trigger(
+      'slideInLeft',
+      [
+        transition(
+          ':enter', [
+            style({transform: 'translateX(100%)', opacity: 0}),
+            animate('500ms', style({transform: 'translateX(-100%)', opacity: 1}))
+          ]
+        ),
+        transition(
+          ':leave', [
+            style({transform: 'translateX(0)', 'opacity': 1}),
+            animate('300ms', style({transform: 'translateX(0)', opacity: 0}))
+          ]
+        )
+      ])
+  ]
 })
 export class AnnouncementsComponent implements OnInit {
   announcements;
-  // announcement;
+  announcement?: Announcement;
+  index = 0;
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     if (!this.announcements) {
       this.announcements = [];
     }
-    this.dataService.getMessages().subscribe((res) => {
-      this.announcements = res;
-    });
+    this.dataService.getMessages()
+      .subscribe(
+        (res) => {
+          this.announcements = res;
+          this.announcement = this.announcements[this.index];
+        },
+        (err) => { console.log('Error: ' + err); },
+        () => {
+          console.log('Completed init');
+        }
+      );
   }
 
-  // cycleMessages() {
-  //   Rx.Observable.interval(2000)
-  //   .takeWhile(i => i < this.announcements.length)
-  //   .map(i => this.announcements[i])
-  //   .subscribe(i => console.log(i));
-  // }
+  onNextAnnouncement() {
+    if (this.index < this.announcements.length - 1) {
+      this.index++;
+    } else {
+      this.index = 0;
+    }
+    this.announcement = this.announcements[this.index];
+  }
+
+  onPrevAnnouncement() {
+    if (this.index > 0) {
+      this.index--;
+    } else {
+      this.index = this.announcements.length - 1;
+    }
+    this.announcement = this.announcements[this.index];
+  }
+
 }
